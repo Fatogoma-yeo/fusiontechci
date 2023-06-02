70a7da2b38aa27e8b2df085112869d8d26e0dba8
Yeo Fatogoma <yeofougnigue8@gmail.com>
2023-06-02 11:09:36 +0000

    navigation file change

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,7 +4,7 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center gap-6 mx-4">
+                <div class="shrink-0 flex items-center gap-2">
                     <button @click="isOpen = !isOpen" class="inline-flex items-center justify-center p-2 rounded-full text-gray-400 hover:text-gray-500 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                         <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                             <path  class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
