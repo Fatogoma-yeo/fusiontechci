c2713c5016699f9a888f520410744eb1146c248d
Yeo Fatogoma <yeofougnigue8@gmail.com>
2023-06-08 14:46:48 +0000

    Many files change

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,8 +16,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('daily:closeApp')->dailyAt('20:00');
-        $schedule->command('daily:openApp')->dailyAt('06:00');
+        $schedule->command('daily:closeApp')->dailyAt('14:48');
+        $schedule->command('daily:openApp')->dailyAt('14:55');
     }
 
     /**
