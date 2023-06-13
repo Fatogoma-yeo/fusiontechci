643724e2a6ff334da95e1c8503b27f2ef57f5edf
Yeo Fatogoma <yeofougnigue8@gmail.com>
2023-06-13 14:21:51 +0000

    orders file change

--- app/Http/Controllers/Dashboard/OrdersController.php
+++ app/Http/Controllers/Dashboard/OrdersController.php
@@ -111,7 +111,7 @@ class OrdersController extends Controller
                                            <input type="text" class="hidden" name="product_id" id="product_id" value='.$procurement->product->id.'  >';
 
                        if($procurement->product->media){
-                           $output .= '<img src='.$procurement->product->media.' class="h-full w-ful" alt="Image Produits" />';
+                           $output .= '<img src='.$procurement->product->media.' class="h-full w-full" alt="Image Produits" />';
                        }else {
                            $output .= '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-full">
                                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
@@ -140,7 +140,7 @@ class OrdersController extends Controller
                                        <input type="text" class="hidden" name="product_id" id="product_id" value='.$product->id.'  >';
 
                    if($product->media){
-                       $output .= '<img src='.$product->media.' class="h-full w-ful" alt="Image Produits" />';
+                       $output .= '<img src='.$product->media.' class="h-full w-full" alt="Image Produits" />';
                    }else {
                        $output .= '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-full">
                                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
@@ -172,7 +172,7 @@ class OrdersController extends Controller
                                            <input type="text" class="hidden" name="product_id" id="product_id" value='.$procurement->product->id.'  >';
 
                        if($procurement->product->media){
-                           $output .= '<img src='.$procurement->product->media.' class="h-full w-ful" alt="Image Produits" />';
+                           $output .= '<img src='.$procurement->product->media.' class="h-full w-full" alt="Image Produits" />';
                        }else {
                            $output .= '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-full">
                                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
@@ -201,7 +201,7 @@ class OrdersController extends Controller
                                        <input type="text" class="hidden" name="product_id" id="product_id" value='.$product->id.'  >';
 
                    if($product->media){
-                       $output .= '<img src='.$product->media.' class="h-full w-ful" alt="Image Produits" />';
+                       $output .= '<img src='.$product->media.' class="h-full w-full" alt="Image Produits" />';
                    }else {
                        $output .= '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-full">
                                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
